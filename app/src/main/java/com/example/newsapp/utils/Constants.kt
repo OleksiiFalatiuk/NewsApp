7df4d01c10b1ea57ab40001ba8b209cd171f93be
Ynikum <alexfalatyuk98@gmail.com>
2022-05-21 15:04:05 +0300

    created models, interfaces with requests and configured retrofit

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "8e3cbf57f688440d9bfe15f65fddaf2e"
+    }
+}
